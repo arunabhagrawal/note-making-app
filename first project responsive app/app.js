a09d638135a7b988211d32781ba1d41313985a0d
arunabhagrawal <74348591+arunabhagrawal@users.noreply.github.com>
2022-02-15 16:02:32 +0530

    idk

--- first project responsive app/app.js
+++
@@ -1,9 +0,0 @@
-var burger = document.querySelector(".burger")
-var navbar = document.querySelector(".navbar")
-
-var navList = document.querySelector(".navlist")
-burger.addEventListener('click',()=>{
-  navList.classList.toggle('v-class-resp')
-  navbar.classList.toggle('h-nav')
-  burger.classList.toggle('')
-})
